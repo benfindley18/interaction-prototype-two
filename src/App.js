d60d706aa03f60c7492f1f8735ae729c01edff05
benfindley18 <ben.kenzieacademy@gmail.com>
2020-07-06 15:20:24 -0400

    created password strength validation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import PasswordStrength from '../src/component/PasswordStrength';
+import '../src/component/PasswordStrength.scss'
 
-function App() {
-  return (<h1></h1>);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      password: '',
+    }
+  }
+
+  render() {
+    const { password } = this.state;
+    return (
+      <div className="App">
+        <div className="meter">
+          <input autoComplete="off" type="password" onChange={e => this.setState({ password: e.target.value })} />
+          <PasswordStrength password={password} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/PasswordStrength.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './PasswordStrength.scss';
+import zxcvbn from 'zxcvbn'
+
+class PasswordStrength extends Component {
+
+    passwordStrength = (result) => {
+        switch (result.score) {
+            case 0:
+                return 'Weak';
+            case 1:
+                return 'Weak';
+            case 2:
+                return 'Fair';
+            case 3:
+                return 'Good';
+            case 4:
+                return 'Strong';
+            default:
+                return 'Weak';
+        }
+    }
+
+    render() {
+        const { password } = this.props;
+        const testedResult = zxcvbn(password);
+        return (
+            <div className="password-strength">
+                <progress
+                    className={`password-strength-progress strength-${this.passwordStrength(testedResult)}`}
+                    value={testedResult.score}
+                    max="4"
+                />
+                <br />
+                <label
+                    className="password-strength-label"
+                >
+                    {password && (
+                        <>
+                            <strong>Password strength:</strong> {this.passwordStrength(testedResult)}
+                        </>
+                    )}
+                </label>
+            </div>
+        );
+    }
+}
+
+export default PasswordStrength;
